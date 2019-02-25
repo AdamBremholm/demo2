28cdac9e69c7b5c4fc1fc3fe09c2e39f24f069fd
AdamBremholm <skiryoland@gmail.com>
2019-02-25 10:41:53 +0100

    Create Product

---
+++ src/main/java/se/iths/martin/demo2/Product.java
@@ -0,0 +1,18 @@
+package se.iths.martin.demo2;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Product {
+
+    private @Id @GeneratedValue long id;
+    private String productName;
+    private String description;
+    private int price;
+
+}
